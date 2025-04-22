d372eac82c451fa2f47ba980748bf8b4c2791828
AbdulAhadMuhammad <ST10365351@vcconnect.edu.za>
2025-04-22 08:36:09 +0200

    git repo testing

--- app/src/main/java/vcmsa/projects/krackshackbanking/MainActivity.kt
+++ app/src/main/java/vcmsa/projects/krackshackbanking/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity()
 {
+    //Imagine imagining
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
