73f3ca2558399210f2844436deeb3fbb0f46dfc8
ST10384544 <ST10384544@vcconnect.edu.za>
2025-04-24 08:15:26 +0200

    configures google auth

---
+++ app/src/main/java/vcmsa/projects/krackshackbanking/Dashboard.kt
@@ -0,0 +1,21 @@
+package vcmsa.projects.krackshackbanking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class Dashboard {
+
+    private lateinit var _data : DatabaseReference
+    private lateinit var _auth : FirebaseAuth
+
+
+    fun srtBudget() {
+
+    }
+
+
+}
