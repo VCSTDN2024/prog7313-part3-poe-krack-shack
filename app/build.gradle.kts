16e23f0bf974aa822da509ba39af4ce8853ec57e
Ryan <ST10301651@vcconnect.edu.za>
2025-04-28 15:09:04 +0200

    Added a progress bar to the dashbaord page. also somewhat fixed the navigation bar.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
+    implementation(libs.androidx.mediarouter)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
