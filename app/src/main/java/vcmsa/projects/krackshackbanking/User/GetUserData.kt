8723375531cb6f6bf0d715d9f067e979b6870141
ST10384544 <ST10384544@vcconnect.edu.za>
2025-05-01 14:45:39 +0200

    fixxed schizo regester user logic

--- app/src/main/java/vcmsa/projects/krackshackbanking/User/GetUserData.kt
+++ app/src/main/java/vcmsa/projects/krackshackbanking/User/GetUserData.kt
@@ -1,4 +1,17 @@
 package vcmsa.projects.krackshackbanking.User
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
+// class to fetch current user
 class GetUserData {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val UID : String = auth.currentUser?.uid.toString()
+    private lateinit var _data : FirebaseDatabase
+
+
+    fun getUserData() {
+
+    }
 }
