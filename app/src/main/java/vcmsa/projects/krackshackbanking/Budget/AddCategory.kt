9bc7debc5ca91fea396c6e3d8b4dfdab5b56fed4
ST10384544 <ST10384544@vcconnect.edu.za>
2025-05-01 17:46:43 +0200

    added start of dialouge box

--- app/src/main/java/vcmsa/projects/krackshackbanking/Budget/AddCategory.kt
+++ app/src/main/java/vcmsa/projects/krackshackbanking/Budget/AddCategory.kt
@@ -1,7 +1,10 @@
 package vcmsa.projects.krackshackbanking.Budget
 
 import android.os.Bundle
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.google.android.gms.common.util.UidVerifier
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
 class AddCategory {
@@ -16,16 +19,46 @@ class AddCategory {
 //       }
 
   private lateinit var _database : FirebaseDatabase
+  private lateinit var _auth : FirebaseAuth
 
 
-  fun getCategory() { // looks for current selected category
 
+  //
+  fun getCategory(ctaegory: String): Boolean { // looks for current selected category
+
+  return false
   }
 
   fun CreateCategory() // if user selects create new category , we go here
   {
 
+    // here we will have a dialouge box to create new category
+
+    var categoryName = ""
+    var categoryID = ""
+    var categoryTotalCost = 0
+    var UID = _auth.currentUser?.uid
+
+    var _catergoy = BudgetModel(categoryID,categoryName, UID.toString(),categoryTotalCost)
+
+
+
+
 
   }
+
+  fun DialougeBox
+    (
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+
+  )
+  {
+
+
+    }
+
 
 }
