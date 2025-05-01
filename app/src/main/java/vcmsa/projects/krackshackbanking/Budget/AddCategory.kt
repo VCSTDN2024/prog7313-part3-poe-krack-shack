6c3c1e4960803eb70bbfbcc0e3217228cdddc410
Dharvish <128782192+Dharvish-Toolsi@users.noreply.github.com>
2025-05-01 22:10:04 +0200

    Changed dialog opener

--- app/src/main/java/vcmsa/projects/krackshackbanking/Budget/AddCategory.kt
+++ app/src/main/java/vcmsa/projects/krackshackbanking/Budget/AddCategory.kt
@@ -1,17 +1,20 @@
 package vcmsa.projects.krackshackbanking.Budget
 
 import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
-
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.google.android.gms.common.util.UidVerifier
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import vcmsa.projects.krackshackbanking.R
 
 class AddCategory {
-
 
 
 //     "category" {
@@ -21,51 +24,46 @@ class AddCategory {
 //         "totalCost": 10000
 //       }
 
-  private lateinit var _database : FirebaseDatabase
-  private lateinit var _auth : FirebaseAuth
+    private lateinit var _database: FirebaseDatabase
+    private lateinit var _auth: FirebaseAuth
 
 
+    //
+    fun getCategory(ctaegory: String): Boolean { // looks for current selected category
 
-  //
-  fun getCategory(ctaegory: String): Boolean { // looks for current selected category
+        return false
+    }
 
-  return false
-  }
+    fun CreateCategory() // if user selects create new category , we go here
+    {
 
-  fun CreateCategory() // if user selects create new category , we go here
-  {
+        // here we will have a dialouge box to create new category
 
-    // here we will have a dialouge box to create new category
+        var categoryName = ""
+        var categoryID = ""
+        var categoryTotalCost = 0
+        var UID = _auth.currentUser?.uid
 
-    var categoryName = ""
-    var categoryID = ""
-    var categoryTotalCost = 0
-    var UID = _auth.currentUser?.uid
+        var _catergoy = BudgetModel(categoryID, categoryName, UID.toString(), categoryTotalCost)
 
-    var _catergoy = BudgetModel(categoryID,categoryName, UID.toString(),categoryTotalCost)
-
-  }
-
-  fun DialougeBox
-    (
-
-  )
-   {
-     /* val builder = AlertDialog.Builder(this)
-      builder.setTitle("New Category")
-      val input = EditText(this)
-      val lp = LinearLayout.LayoutParams(
-        LinearLayout.LayoutParams.MATCH_PARENT,
-        LinearLayout.LayoutParams.MATCH_PARENT
-      )
-      input.layoutParams = lp
-      builder.setView(input)*/
 
     }
-    class UpdateCategory {
 
-    }
-    class DeleteCategory {
-
+    fun openDialog(context: Context, onSubmit: (String) -> Unit) {
+        val dialog = AlertDialog.Builder(context).create()
+        val view = LayoutInflater.from(context).inflate(R.layout.category_dialogue, null)
+        val categoryNameEditText = view.findViewById<EditText>(R.id.txtCategoryInput)
+        view.findViewById<Button>(R.id.btnCancel).setOnClickListener {
+            dialog.dismiss()
+        }
+        view.findViewById<Button>(R.id.btnEnter).setOnClickListener {
+            val categoryName = categoryNameEditText.text.toString()
+            if (categoryName.isNotEmpty()) {
+                onSubmit(categoryName)
+                dialog.dismiss()
+            }
+        }
+        dialog.setView(view)
+        dialog.show()
     }
 }
