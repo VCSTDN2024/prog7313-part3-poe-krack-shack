cb64a6d0e8b7f32a27c6e83882ceed57c8221a84
AbdulAhadMuhammad <ST10365351@vcconnect.edu.za>
2025-06-05 16:06:35 +0200

    added extension to androidmanifest

--- app/src/main/java/vcmsa/projects/krackshackbanking/Expense/ExpenseHandler.kt
+++ app/src/main/java/vcmsa/projects/krackshackbanking/Expense/ExpenseHandler.kt
@@ -1,5 +1,6 @@
 package vcmsa.projects.krackshackbanking.Expense
 
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -7,7 +8,7 @@ import kotlinx.coroutines.tasks.await
 import java.util.Date
 
 
- class ExpenseHandler {
+ class ExpenseHandler : AppCompatActivity() {
 
 
 }
